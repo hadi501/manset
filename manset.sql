0ba6c0f45f43ddf708906c596b8e871f176df2c1
Hadi501 <naxsulaxsanahadi@gmail.com>
2023-11-08 11:21:25 +0700

    changes database

--- manset.sql
+++ manset.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 28, 2023 at 09:58 AM
+-- Generation Time: Nov 08, 2023 at 05:10 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -49,6 +49,14 @@ CREATE TABLE `employes` (
   `created_at` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `employes`
+--
+
+INSERT INTO `employes` (`id`, `name`, `task`, `phone`, `updated_at`, `created_at`) VALUES
+(1, 'Abdul', '1', '089284750739', '2023-10-22', '2023-10-22'),
+(2, 'Martina', '3', '085784650387', '2023-10-22', '2023-10-22');
+
 -- --------------------------------------------------------
 
 --
@@ -67,6 +75,13 @@ CREATE TABLE `finishing` (
   `created_at` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `finishing`
+--
+
+INSERT INTO `finishing` (`id`, `order_id`, `employe_id`, `task`, `amount`, `date`, `status`, `updated_at`, `created_at`) VALUES
+(1, 15, 2, '3', 180, '2023-10-22', '0', '2023-10-22', '2023-10-22');
+
 -- --------------------------------------------------------
 
 --
@@ -76,17 +91,28 @@ CREATE TABLE `finishing` (
 CREATE TABLE `orders` (
   `id` bigint(20) NOT NULL,
   `customer` varchar(150) NOT NULL,
-  `sock` varchar(50) NOT NULL,
-  `color` varchar(20) NOT NULL,
+  `sock` varchar(100) NOT NULL,
+  `color` varchar(100) NOT NULL,
   `size` varchar(10) NOT NULL,
   `amount` int(11) NOT NULL,
   `date` date NOT NULL,
   `deadline` date NOT NULL,
-  `price` bigint(20) NOT NULL,
   `status` enum('0','1') NOT NULL,
   `updated_at` date DEFAULT NULL,
   `created_at` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `customer`, `sock`, `color`, `size`, `amount`, `date`, `deadline`, `status`, `updated_at`, `created_at`) VALUES
+(14, 'Fulan', 'Jempol Polos', 'Hitam', '30', 120, '2023-10-16', '2023-10-22', '0', '2023-10-15', '2023-10-15'),
+(15, 'Fulanah', 'Mensock', 'Abu Tua', '30x25', 240, '2023-10-21', '2023-10-28', '0', '2023-10-22', '2023-10-21'),
+(16, 'Fulan', 'Mensock', 'Abu Tua', '30x25', 240, '2023-10-21', '2023-10-28', '0', '2023-10-21', '2023-10-21'),
+(17, 'Fulan', 'School', 'Putih', '29x25', 600, '2023-10-20', '2023-11-03', '0', '2023-10-21', '2023-10-21'),
+(18, 'Azkia', 'Kaos Kaki Sambung', 'Abu Muda Tulisan Hitam', '45', 72, '2023-10-22', '2023-10-29', '0', '2023-10-23', '2023-10-23'),
+(19, 'Azkia', 'Kaos Kaki Sambung', 'Ungu Tua Tulisan Putih', '45', 72, '2023-10-22', '2023-10-29', '0', '2023-10-23', '2023-10-23');
 
 -- --------------------------------------------------------
 
@@ -106,6 +132,15 @@ CREATE TABLE `productions` (
   `updated_at` date DEFAULT NULL,
   `created_at` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `productions`
+--
+
+INSERT INTO `productions` (`id`, `order_id`, `employe_id`, `shift`, `machine_no`, `amount`, `date`, `status`, `updated_at`, `created_at`) VALUES
+(1, 14, 1, '0', 1, 60, '2023-10-22', '0', '2023-10-22', '2023-10-22'),
+(2, 15, 1, '1', 1, 180, '2023-10-23', '0', '2023-10-22', '2023-10-22'),
+(3, 18, 1, '0', 1, 72, '2023-10-23', '0', '2023-10-23', '2023-10-23');
 
 -- --------------------------------------------------------
 
@@ -178,25 +213,25 @@ ALTER TABLE `colors`
 -- AUTO_INCREMENT for table `employes`
 --
 ALTER TABLE `employes`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `finishing`
 --
 ALTER TABLE `finishing`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `productions`
 --
 ALTER TABLE `productions`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `socks`
